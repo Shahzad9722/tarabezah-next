b981fcd0a4c0dc1874baf1945a493bf7f0d4c550
Shahzad Hussain <shezijamal@gmail.com>
2025-04-19 14:05:41 +0500

    added update

--- app/components/headerWrapper.tsx
+++ app/components/headerWrapper.tsx
@@ -5,7 +5,7 @@ import Header from "./Header/Header";
 
 export default function HeaderWrapper() {
     const pathname = usePathname();
-    const hideHeader = pathname === "/reservation" || pathname === "/walk-in"
+    const hideHeader = pathname === "/reservation" || pathname === "/walk-in" || pathname === "/login"
 
     if (hideHeader) return null;
     return <Header />;
