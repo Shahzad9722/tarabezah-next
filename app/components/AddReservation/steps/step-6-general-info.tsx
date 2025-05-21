39988c0af8cfd84585d09d8f70641465f478a89d
AreebYounus4 <areeb@0degrees.io>
2025-05-22 02:54:47 +0500

    updated  some or more

--- app/components/AddReservation/steps/step-6-general-info.tsx
+++ app/components/AddReservation/steps/step-6-general-info.tsx
@@ -13,7 +13,7 @@ export default function GeneralInfoStep({
   tags: { name: string; value: number; iconUrlWhite: string, iconUrlGold: string }[];
 }) {
   return (
-    <div className='w-full'>
+    <div className='w-full between-area'>
       <h2 className='text-color-E9E3D7 text-[22px] font-semibold mb-6'>General Information</h2>
 
       {/* Tags Select Options */}
