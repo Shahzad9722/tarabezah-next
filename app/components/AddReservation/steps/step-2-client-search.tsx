0a18ca790a2371aa8b77c4f13409e8899503e6f0
Shahzad Hussain <shezijamal@gmail.com>
2025-05-23 17:05:15 +0500

    added fix

--- app/components/AddReservation/steps/step-2-client-search.tsx
+++ app/components/AddReservation/steps/step-2-client-search.tsx
@@ -114,6 +114,7 @@ export default function AddReservationStep({
                     type='date'
                     name='birthday'
                     max={new Date().toISOString().split('T')[0]}
+                    placeholder="dd/mm/yyyy"
                     {...field}
                     className={cn(
                       'flex h-10 w-full rounded-md border border-input bg-color-222036 px-3 border-color-222036 py-2 text-base ring-offset-background text-color-E9E3D7 file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm pr-10'
